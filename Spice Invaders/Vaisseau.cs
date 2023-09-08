c6908149a2a67a96b1f049fc62c97656806a3e92
joaberch <joachimsimeongabriel.berchel@eduvaud.ch>
2023-09-08 11:26:50 +0200

    avancement léger sur le code

---
+++ Spice Invaders/Vaisseau.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Spice_Invaders
+{
+    internal class Vaisseau
+    {
+        public string[] withoutParachute =                  //Sprite du parachutiste sans parachute
+{
+         @"     ",
+         @"     ",
+         @"     ",
+         @"  o  ",
+         @" /░\ ",
+         @" / \ ",
+     };
+        public Vaisseau(int vieVaisseau)
+        {
+
+        }
+
+    }
+}
