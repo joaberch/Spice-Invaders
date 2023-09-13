2a3f165b6ac1e649449e01c98a8393fc42fc3d9a
joaberch <joachimsimeongabriel.berchel@eduvaud.ch>
2023-09-13 10:17:03 +0200

    feat(Ammunition)
    Now the player can exit the programm with escape.
    He can also move with left or right arrow and shoot a projectile with space bar.
    There is a new class the ammunition

--- Spice Invaders/Program.cs
+++ Spice Invaders/Program.cs
@@ -1,8 +1,52 @@
 ﻿using Spice_Invaders;
 
-Vaisseau joueur = new Vaisseau();
+Vaisseau joueur = new Vaisseau(10);
 
-while(true)
+//Assignation des variables (TODO : rename all comments in english)
+Console.CursorVisible = false;
+ConsoleKeyInfo keyPressed;      //Préparer la variable pour récupérer la saisie
+//TODO : collection ennemi mort
+List<Ammunition> shooted = new List<Ammunition>(); 
+
+
+
+//Menu
+
+while (true)     //game engine
 {
+    Console.Clear();    //Nettoyer l'écran
+    joueur.show();      //Afficher le joueur
 
+    foreach(Ammunition ammo in shooted)
+    {
+        ammo.show();
+        ammo.move();
+    }
+
+    if (Console.KeyAvailable)                               // L'utilisateur a pressé une touche
+    {
+
+        keyPressed = Console.ReadKey(false);
+        switch (keyPressed.Key)
+        {
+            case ConsoleKey.LeftArrow:                       //Si l'utilisateur a appuyé sur flèche de gauche
+                joueur.MovingLeft();
+                break;
+
+            case ConsoleKey.RightArrow:                     //Si l'utilisateur a appuyé sur la flèche de droite
+                joueur.MovingRight();
+                break;
+
+            case ConsoleKey.Spacebar:                       //if the player is pressing the spacebar
+                joueur.Shoot();
+                shooted.Add(new Ammunition(joueur.x, joueur.y));
+                break;
+
+            case ConsoleKey.Escape:                         //Si l'utilisateur a appuyé sur exit
+                Environment.Exit(0);                         //Quitter le programme
+                break;
+        }
+    }
+
+    Thread.Sleep(50);       //equivalent to 60fps (TODO: check)
 }
--- Spice Invaders/Vaisseau.cs
+++ Spice Invaders/Vaisseau.cs
@@ -8,18 +8,57 @@ namespace Spice_Invaders
 {
     internal class Vaisseau
     {
-        public string[] withoutParachute =                  //Sprite du parachutiste sans parachute
+        public int x = 0;
+        public int y = Console.WindowHeight-HAUTEURVAISSEAU;
+        const int HAUTEURVAISSEAU = 4;
+        public string[] PlayerSprite =                      //Sprite du parachutiste sans parachute
 {
-         @"     ",
-         @"     ",
-         @"     ",
-         @"  o  ",
-         @" /░\ ",
-         @" / \ ",
+            @"   __",
+            @"  /__\",
+            @" /(__)\",
+            @"(__)(__)",
      };
-        public Vaisseau(int vieVaisseau)
+        public Vaisseau(int vieVaisseau)                    //Constructeur 1 (ennemi faible)
         {
 
+        }
+
+        public void show()                                  //Afficher le vaisseau du joueur
+        {
+            for (int i = 0; i < HAUTEURVAISSEAU; ++i)
+            {
+                Console.SetCursorPosition(x, Console.WindowHeight-HAUTEURVAISSEAU+i);   //Sélectionne le bas de l'écran + 1
+                Console.WriteLine(PlayerSprite[i]);
+            }
+        }
+
+        public void MovingLeft()
+        {
+            if(x <= 1)
+            {
+
+            }
+            else
+            {
+                --x;
+            }
+        }
+
+        public void MovingRight()
+        {
+            if (x >= Console.WindowWidth-4-PlayerSprite.Length)
+            {
+
+            }
+            else
+            {
+                ++x;
+            }
+        }
+
+        public void Shoot()
+        {
+            Ammunition ammo = new Ammunition(x, y);
         }
 
     }
